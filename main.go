0f28a877e5ad6b10c6c9dc086c2d96e368366ccb
Jon Narine <jon.wolf.narine@gmail.com>
2026-09-12 06:55:13 -0400

    create structure for todo

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello Go World!")
+	fmt.Println("###### Hello Go World!")
+	fmt.Println("1. Watch Go crash course")
+	fmt.Println("2. Watch Nana's Go Full Course")
+	fmt.Println("3. Reward myself with a cheesecake")
 }
