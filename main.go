7162591a6744258d0a11b7852a68a79577a7d5c6
Jon Narine <jon.wolf.narine@gmail.com>
2026-09-16 11:23:30 -0400

    add toggle, edit and print funcs then render table

--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
 	todos := Todos{}
 	todos.add("but milk")
 	todos.add("but bread")
-	fmt.Printf("%+v\n\n", todos)
-	todos.delete(0)
-	fmt.Printf("%+v", todos)
+	todos.toggle(0)
+	todos.print()
 }
--- todo.go
+++ todo.go
@@ -1,7 +1,13 @@
 package main
 
-import "time"
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"time"
+	"os"
+	"github.com/aquasecurity/table"
+	"strconv"
+)
 
 type Todo struct {
 	Title string
@@ -44,4 +50,64 @@ func (todos *Todos) delete(index int) error {
 	*todos = append(t[:index], t[index+1:]...)
 
 	return nil
+}
+
+func (todos *Todos) toggle(index int) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	isCompleted := t[index].Completed
+
+	if !isCompleted {
+		completionTime := time.Now()
+		t[index].CompletedAt = &completionTime
+	}
+
+	t[index].Completed = !isCompleted
+
+	return nil
+}
+
+func (todos *Todos) edit(index int, title string) error {
+	t := *todos
+
+	if err := t.validateIndex(index); err != nil {
+		return err
+	}
+
+	t[index].Title = title
+
+	return nil
+}
+
+func (todos *Todos) print() {
+	table := table.New(os.Stdout)
+	table.SetRowLines(false)
+	table.SetHeaders("#", "Title", "Completed", "CreatedAt", "CompletedAt")
+
+	for index, t := range *todos {
+		completed := "❌"
+		completedAt := ""
+		
+		if t.Completed {
+			completed = "✅"
+
+			if t.CompletedAt != nil {
+				completedAt = t.CompletedAt.Format(time.RFC1123)
+			}
+		}
+		
+		table.AddRow(
+			strconv.Itoa(index),
+			t.Title,
+			completed,
+			t.CreatedAt.Format(time.RFC1123),
+			completedAt,
+		)
+	}
+
+	table.Render()
 }
